527f7768f607c4dc04bb1bdd70908ea11dd34c4f
Louis Lecouturier <71454059+LouisLecouturier@users.noreply.github.com>
2024-10-07 08:57:28 +0200

    fixed ?

--- infra/python-backend/main.tf
+++ infra/python-backend/main.tf
@@ -8,12 +8,6 @@ resource "azurerm_service_plan" "tp2Plan" {
   sku_name            = "B1"
 }
 
-data "archive_file" "tp2PythonApp" {
-  type        = "zip"
-  source_dir  = "${path.module}/../../python/"
-  output_path = "${path.module}/python_app.zip"
-  excludes    = ["__pycache__", "*.pyc", "*.pyo", ".venv"]
-}
 
 resource "azurerm_linux_web_app" "tp2PythonApp" {
   name                = "tp2PythonApp"
@@ -21,22 +15,17 @@ resource "azurerm_linux_web_app" "tp2PythonApp" {
   location            = var.location
   service_plan_id     = azurerm_service_plan.tp2Plan.id
 
-
-
   site_config {
     application_stack {
-      python_version = "3.12"
+      python_version = "3.9"
     }
-    always_on        = true
-    app_command_line = "startup.txt"
-  }
+    always_on = true
 
-  zip_deploy_file = data.archive_file.tp2PythonApp.output_path
+  }
 
   app_settings = {
     SCM_DO_BUILD_DURING_DEPLOYMENT = "true"
     WEBSITE_RUN_FROM_PACKAGE       = "1"
-    STARTUP_COMMAND                = "uvicorn main:app --host 0.0.0.0 --port 8000"
   }
 
   timeouts {
@@ -50,7 +39,4 @@ resource "azurerm_app_service_source_control" "tp2SourceControl" {
   repo_url               = "https://github.com/LouisLecouturier/cloud-computing-tp.git"
   branch                 = "master"
   use_manual_integration = true
-  use_mercurial          = false
-
-
 }
